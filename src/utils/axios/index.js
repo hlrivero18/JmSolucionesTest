5cfef75c9319912c097fae07273b4868dcda6093
Héctor Luis Rivero <hectorluisrivero14@gmail.com>
2023-12-02 16:37:14 -0300

    corrigiendo endpoint

--- src/utils/axios/index.js
+++ src/utils/axios/index.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create(
     {
-        baseURL: 'http://localhost:3000/api'
+        // baseURL: 'http://localhost:3000/api'
+        baseURL: 'https://jm-soluciones-test.vercel.app/api'
     }
 )
 
