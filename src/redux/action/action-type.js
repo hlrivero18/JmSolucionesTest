eb6294ea33f4c3c01350468843e392da85fac1ed
Héctor Luis Rivero <hectorluisrivero14@gmail.com>
2023-12-03 15:54:36 -0300

    removiendo archivos innecesarios

--- src/redux/action/action-type.js
+++
@@ -1,5 +0,0 @@
-export const ALL_LIST = 'ALL_LIST'
-export const ADD_PERSON = 'ADD_PERSON';
-export const DELETE_PERSON = 'DELETE_PERSON';
-export const EDIT_PERSON = 'EDIT_PERSON';
-export const SEARCH_PERSON = 'SEARCH_PERSON';
